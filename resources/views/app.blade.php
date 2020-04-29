832ec2be3f3c16fac44ba2f4844154ac753b4554
Damian <damianbiaek1997@hotmail.com>
2020-04-29 17:46:05 +0200

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Panel administratora - fluffy</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <router-view></router-view>
+</div>
+<script src="{{mix("/js/app.js")}}"></script>
+</body>
