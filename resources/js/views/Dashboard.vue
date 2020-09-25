1447f420d9f1c0a8dd9f2adab719ce1df8d1f980
Damian <damianbiaek1997@hotmail.com>
2020-09-25 20:52:18 +0200

    Refactoring and fix working on shared hosting

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -8,25 +8,8 @@
 </template>
 
 <script>
-    import axios from 'axios';
-
     export default {
         name: "Dashboard",
-        data() {
-            return {
-                user: {}
-            }
-        },
-        computed: {
-            token() {
-                return this.$store.getters.currentUserToken;
-            }
-        },
-        mounted() {
-            axios.post("/api/auth/me").then(res => {
-                this.user = res.data;
-            })
-        }
     }
 </script>
 
