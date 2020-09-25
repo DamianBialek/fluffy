821004d9fd4d3193304ee67cab23eb6eb79f6340
Damian <damianbiaek1997@hotmail.com>
2020-09-25 16:57:09 +0200

    Add basic views to dashboard, calendar and configuration

---
+++ resources/js/views/Calendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page">
+        <section>
+            <p class="text-center">Tutaj pojawi się kalendarz/terminarz</p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+name: "Calendar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Configuration.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page">
+        <section>
+            <p class="text-center">Tutaj pojawi się konfiguracja całej aplikacji</p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Configuration"
+}
+</script>
+
+<style scoped>
+
+</style>
